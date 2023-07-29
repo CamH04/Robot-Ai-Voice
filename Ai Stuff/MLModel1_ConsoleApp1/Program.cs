60ef20f5b3357fa7ce317e0518b228c8a88a49ca
Cam <cameronahaynes04@gmail.com>
2023-07-29 12:09:56 +0100

    writes to arduino

--- Ai Stuff/MLModel1_ConsoleApp1/Program.cs
+++ Ai Stuff/MLModel1_ConsoleApp1/Program.cs
@@ -1,28 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO.Ports;
 using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.IO.Ports;
 
 namespace MLModel1_ConsoleApp1
 {
-    using OpenQA.Selenium;
-    using OpenQA.Selenium.Chrome;
-    using System.IO.Ports;
     class Program
     {
         static void Main(string[] args)
         {
+            // setting up Web Scraper stuff
             IWebDriver driver = new ChromeDriver();
             driver.Navigate().GoToUrl("https://deft-jelly-f65237.netlify.app");
+
             while (true)
             {
+                // every 2 seconds it scrapes website, finds element and goes through sentiment anylsis
                 string element = driver.FindElement(By.Id("convert_text")).Text;
                 Console.WriteLine("Website Read: " + Convert.ToString(element));
                 Ai(element);
                 Thread.Sleep(2000);
+
             }
         }
         static void Ai(string elementRead)
@@ -34,12 +33,37 @@ namespace MLModel1_ConsoleApp1
             };
             // Make a single prediction on the sample data and print results
             var Result = MLModel1.Predict(sampleData);
-
-
             // 0 is bad
             // 1 is good
             Console.WriteLine($"Result: " + Result.Prediction);
             Console.WriteLine("-------------------------------------");
+
+
+
+            // setting up I/O stuff
+            SerialPort serialPort;
+            //com port, baudrate
+            serialPort = new SerialPort("COM3", 9600);
+            //opens data transmition
+            try
+            {
+                serialPort.Open();
+            }
+            catch
+            {
+                Console.WriteLine("Somthings went wrong (COM PORT)");
+            }
+            // writes to arduino
+            if(Convert.ToInt32(Result) == 1)
+            {
+                serialPort.Write("h");
+                Thread.Sleep(700);
+                serialPort.Write("n");
+            }
+            if (Convert.ToInt32(Result) == 0)
+            {
+                serialPort.Write("s");
+            }
         }
     }
 }
